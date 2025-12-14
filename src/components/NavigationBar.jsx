ad96827c2ff8336a07bd4c9909b0df3ce02a7c62
Vishal Sudasinghe <oshadavishal392@gmail.com>
2025-12-14 15:54:45 +0530

    Refactor NavigationBar component to use NavLink for routing and update CSS for improved styling

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const NavigationBar = () => {
   return (
     <nav className="floating-navbar">
       <ul className="nav-list">
-        <li className="active">Home</li>
-        <li>Search</li>
-        <li>About us</li>
-        <li>Contact</li>
+        <li>
+            <NavLink to="/" end>Home</NavLink>
+        </li>
+        <li>
+            <NavLink to="/search">Search</NavLink>
+        </li>
+        <li>
+            <NavLink to="/about">About us</NavLink>
+        </li>
+        <li>
+            <NavLink to="/contact">Contact</NavLink>
+        </li>
       </ul>
     </nav>
   )
